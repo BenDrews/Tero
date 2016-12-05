c15cb09f394844d6f9b6be5c787bb8c1c7c6ec29
drg4 <DiegoRGonzalez@users.noreply.github.com>
2016-12-05 06:09:11 +0000

    Changed animation controller and started working on shockwave

--- source/AnimationControl.cpp
+++ source/AnimationControl.cpp
@@ -5,13 +5,17 @@ AnimationControl::AnimationControl() {
 	m_currentTime = 0.0f;
 }
 
-AnimationControl::AnimationControl(std::function<void(SimTime, SimTime, shared_ptr<Table<String, float>>)> c) {
+
+AnimationControl::AnimationControl( std::function<void(SimTime, SimTime, shared_ptr<Table<String, float>>, Table< Point3int32, shared_ptr<VisibleEntity>>&, Table< Point3int32, SimTime>& )> c) {
+
 	m_currentTime = 0.0f;
 	m_callback = c;
+
 	args = std::make_shared<Table<String,float>>(Table<String,float>());
+
 }
 
 void AnimationControl::invoke(SimTime sdt) {
 	m_currentTime += sdt;
-	m_callback(sdt, m_currentTime, args);
+	m_callback(sdt, m_currentTime, args, activeVoxels, timeAdded);
 }
